6f87c0bdcec49df6fa272af30a0e55cb5707eb07
xuanngo10 <xuan.ngo10@gmail.com>
2021-02-19 10:39:05 -0800

    added more form info

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -11,6 +11,40 @@ function App() {
           <input name='email' placeholder='Email' type='text'></input>
           <input name='phone' placeholder='Phone Number' type='tel'></input>
         </form>
+        <form>
+          <input name='address' placeholder='Address' type='text'></input>
+          <input name='city' placeholder='City' type='text'></input>
+          <input name='state' placeholder='State' type='text'></input>
+          <input name='zip' placeholder='Zip' type='text'></input>
+        </form>
+      </div>
+      <div>
+        <h2>Resume objective</h2>
+        <form>
+          <textarea
+            name='objective'
+            placeholder='Summarize your career goals'
+          ></textarea>
+        </form>
+      </div>
+      <div>
+        <h2>Education</h2>
+        <form>
+          <input
+            name='school'
+            placeholder='Name of school or organization'
+            type='text'
+          ></input>
+          <input name='degree' placeholder='Degree title' type='text'></input>
+          <input name='from-date' placeholder='From' type='date'></input>
+          <input name='to-date' placeholder='To' type='date'></input>
+        </form>
+        <button>
+          <span>
+            <i class='fas fa-plus'></i>
+          </span>
+          Add
+        </button>
       </div>
     </div>
   );

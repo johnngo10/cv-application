8f248f4f5a3f01b20853b63c3e868642e4ae5f83
xuanngo10 <xuan.ngo10@gmail.com>
2021-02-22 16:02:07 -0800

    added styling for forms

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -6,21 +6,37 @@ function App() {
       <div id='pesonal-info-form-container'>
         <h2>Personal Information</h2>
         <form>
-          <input name='first-name' placeholder='First Name' type='text'></input>
-          <input name='last-name' placeholder='Last Name' type='text'></input>
+          <input
+            name='first-name'
+            placeholder='First Name'
+            type='text'
+            required
+          ></input>
+          <input
+            name='last-name'
+            placeholder='Last Name'
+            type='text'
+            required
+          ></input>
           <input name='email' placeholder='Email' type='text'></input>
           <input
             name='phone'
             placeholder='Phone Number'
             type='tel'
             pattern='[0-9]{3}-[0-9]{2}-[0-9]{3}'
+            required
           ></input>
         </form>
         <form>
-          <input name='address' placeholder='Address' type='text'></input>
-          <input name='city' placeholder='City' type='text'></input>
-          <input name='state' placeholder='State' type='text'></input>
-          <input name='zip' placeholder='Zip' type='text'></input>
+          <input
+            name='address'
+            placeholder='Address'
+            type='text'
+            required
+          ></input>
+          <input name='city' placeholder='City' type='text' required></input>
+          <input name='state' placeholder='State' type='text' required></input>
+          <input name='zip' placeholder='Zip' type='text' required></input>
         </form>
       </div>
       <div id='resume-obj-container'>
@@ -29,30 +45,42 @@ function App() {
           <textarea
             name='objective'
             placeholder='Summarize your career goals'
+            required
           ></textarea>
         </form>
       </div>
       <div id='experience-container'>
-        <h2>EXPERIENCE</h2>
+        <h2>Experience</h2>
         <form>
           <input
             name='title'
             placeholder='Title'
             type='text'
             class='experience-fields'
+            required
           ></input>
           <input
             name='company-name'
             placeholder='Company'
             type='text'
             class='experience-fields'
+            required
           ></input>
           <input
-            placeholder='From'
+            placeholder='From:'
             type='date'
-            class='experience-fields'
+            class='experience-fields from-date'
+            required
           ></input>
-          <input placeholder='To' type='date' class='experience-fields'></input>
+          <input
+            placeholder='To:'
+            type='date'
+            class='experience-fields to-date'
+          ></input>
+          <label class='switch'>
+            <input type='checkbox' class='toggle-ongoing'></input>
+            <span class='slider round'>Present</span>
+          </label>
           <input
             name='title'
             placeholder='Activities & Responsiblities'
@@ -94,10 +122,30 @@ function App() {
             name='school'
             placeholder='Name of school or organization'
             type='text'
+            class='education-input'
           ></input>
-          <input name='degree' placeholder='Degree title' type='text'></input>
-          <input name='from-date' placeholder='From' type='date'></input>
-          <input name='to-date' placeholder='To' type='date'></input>
+          <input
+            name='degree'
+            placeholder='Degree title'
+            type='text'
+            class='education-input'
+          ></input>
+          <input
+            name='from-date'
+            placeholder='From:'
+            type='date'
+            class='education-input from-date'
+          ></input>
+          <input
+            name='to-date'
+            placeholder='To:'
+            type='date'
+            class='to-date'
+          ></input>
+          <label class='switch'>
+            <input type='checkbox' class='toggle-ongoing'></input>
+            <span class='slider round'>Present</span>
+          </label>
         </form>
         <button id='add-education-button'>
           <span>
@@ -106,12 +154,18 @@ function App() {
           Add
         </button>
       </div>
-      <div>
+      <div id='cert-container'>
         <h2>Licenses & Certificates</h2>
         <form>
           <input placeholder='name' type='text'></input>
           <input placeholder='Date of completion' type='date'></input>
         </form>
+        <button id='add-education-button'>
+          <span>
+            <i class='fas fa-plus'></i>
+          </span>
+          Add
+        </button>
       </div>
     </div>
   );
